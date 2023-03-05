4980e6a1e901356c69fc7a6bc4ed2e4db4252203
Leather128 <81660872+Leather128@users.noreply.github.com>
2023-03-05 18:22:19 -0500

    ok nvm NOW it's done (cuz defines kick my ass)

--- src/al/ALC.hx
+++ src/al/ALC.hx
@@ -11,6 +11,34 @@ import cpp.Pointer;
 @:buildXml('<include name="../../build/build.xml" />')
 @:include('AL/alc.h')
 extern class ALC {
+	// defines //
+	public static inline var ALC_FREQUENCY:Int = 0x1007;
+	public static inline var ALC_REFRESH:Int = 0x1008;
+	public static inline var ALC_SYNC:Int = 0x1009;
+	public static inline var ALC_MONO_SOURCES:Int = 0x1010;
+	public static inline var ALC_STEREO_SOURCES:Int = 0x1011;
+	public static inline var ALC_NO_ERROR:Int = 0;
+	public static inline var ALC_INVALID_DEVICE:Int = 0xA001;
+	public static inline var ALC_INVALID_CONTEXT:Int = 0xA002;
+	public static inline var ALC_INVALID_ENUM:Int = 0xA003;
+	public static inline var ALC_INVALID_VALUE:Int = 0xA004;
+	public static inline var ALC_OUT_OF_MEMORY:Int = 0xA005;
+	public static inline var ALC_MAJOR_VERSION:Int = 0x1000;
+	public static inline var ALC_MINOR_VERSION:Int = 0x1001;
+	public static inline var ALC_ATTRIBUTES_SIZE:Int = 0x1002;
+	public static inline var ALC_ALL_ATTRIBUTES:Int = 0x1003;
+	public static inline var ALC_DEFAULT_DEVICE_SPECIFIER:Int = 0x1004;
+	public static inline var ALC_DEVICE_SPECIFIER:Int = 0x1005;
+	public static inline var ALC_EXTENSIONS:Int = 0x1006;
+	public static inline var ALC_EXT_CAPTURE:Int = 1;
+	public static inline var ALC_CAPTURE_DEVICE_SPECIFIER:Int = 0x310;
+	public static inline var ALC_CAPTURE_DEFAULT_DEVICE_SPECIFIER:Int = 0x311;
+	public static inline var ALC_CAPTURE_SAMPLES:Int = 0x312;
+	public static inline var ALC_ENUMERATE_ALL_EXT:Int = 1;
+	public static inline var ALC_DEFAULT_ALL_DEVICES_SPECIFIER:Int = 0x1012;
+	public static inline var ALC_ALL_DEVICES_SPECIFIER:Int = 0x1013;
+
+	// functions //
 	@:native('alcOpenDevice')
 	public static function openDevice(?deviceName:String):Device;
 

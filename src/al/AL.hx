554f06d85e6fc7c8561266cff3c3bc590f5f8b47
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-18 06:46:52 +0000

    Fixed some types

--- src/al/AL.hx
+++ src/al/AL.hx
@@ -9,71 +9,71 @@ package al;
 @:include('AL/al.h')
 extern class AL {
 	// defines //
-	public static inline var NONE:Int = 0;
-	public static inline var FALSE:Int = 0;
-	public static inline var TRUE:Int = 1;
-	public static inline var SOURCE_RELATIVE:Int = 0x202;
-	public static inline var CONE_INNER_ANGLE:Int = 0x1001;
-	public static inline var CONE_OUTER_ANGLE:Int = 0x1002;
-	public static inline var PITCH:Int = 0x1003;
-	public static inline var POSITION:Int = 0x1004;
-	public static inline var DIRECTION:Int = 0x1005;
-	public static inline var VELOCITY:Int = 0x1006;
-	public static inline var LOOPING:Int = 0x1007;
-	public static inline var BUFFER:Int = 0x1009;
-	public static inline var GAIN:Int = 0x100A;
-	public static inline var MIN_GAIN:Int = 0x100D;
-	public static inline var MAX_GAIN:Int = 0x100E;
-	public static inline var ORIENTATION:Int = 0x100F;
-	public static inline var SOURCE_STATE:Int = 0x1010;
-	public static inline var INITIAL:Int = 0x1011;
-	public static inline var PLAYING:Int = 0x1012;
-	public static inline var PAUSED:Int = 0x1013;
-	public static inline var STOPPED:Int = 0x1014;
-	public static inline var BUFFERS_QUEUED:Int = 0x1015;
-	public static inline var BUFFERS_PROCESSED:Int = 0x1016;
-	public static inline var REFERENCE_DISTANCE:Int = 0x1020;
-	public static inline var ROLLOFF_FACTOR:Int = 0x1021;
-	public static inline var CONE_OUTER_GAIN:Int = 0x1022;
-	public static inline var MAX_DISTANCE:Int = 0x1023;
-	public static inline var SEC_OFFSET:Int = 0x1024;
-	public static inline var SAMPLE_OFFSET:Int = 0x1025;
-	public static inline var BYTE_OFFSET:Int = 0x1026;
-	public static inline var SOURCE_TYPE:Int = 0x1027;
-	public static inline var STATIC:Int = 0x1028;
-	public static inline var STREAMING:Int = 0x1029;
-	public static inline var UNDETERMINED:Int = 0x1030;
-	public static inline var FORMAT_MONO8:Int = 0x1100;
-	public static inline var FORMAT_MONO16:Int = 0x1101;
-	public static inline var FORMAT_STEREO8:Int = 0x1102;
-	public static inline var FORMAT_STEREO16:Int = 0x1103;
-	public static inline var FREQUENCY:Int = 0x2001;
-	public static inline var BITS:Int = 0x2002;
-	public static inline var CHANNELS:Int = 0x2003;
-	public static inline var SIZE:Int = 0x2004;
-	public static inline var UNUSED:Int = 0x2010;
-	public static inline var PENDING:Int = 0x2011;
-	public static inline var PROCESSED:Int = 0x2012;
-	public static inline var NO_ERROR:Int = 0;
-	public static inline var INVALID_NAME:Int = 0xA001;
-	public static inline var INVALID_ENUM:Int = 0xA002;
-	public static inline var INVALID_VALUE:Int = 0xA003;
-	public static inline var INVALID_OPERATION:Int = 0xA004;
-	public static inline var OUT_OF_MEMORY:Int = 0xA005;
-	public static inline var VENDOR:Int = 0xB001;
-	public static inline var VERSION:Int = 0xB002;
-	public static inline var RENDERER:Int = 0xB003;
-	public static inline var EXTENSIONS:Int = 0xB004;
-	public static inline var DOPPLER_FACTOR:Int = 0xC000;
-	public static inline var DOPPLER_VELOCITY:Int = 0xC001;
-	public static inline var SPEED_OF_SOUND:Int = 0xC003;
-	public static inline var DISTANCE_MODEL:Int = 0xD000;
-	public static inline var INVERSE_DISTANCE:Int = 0xD001;
-	public static inline var INVERSE_DISTANCE_CLAMPED:Int = 0xD002;
-	public static inline var LINEAR_DISTANCE:Int = 0xD003;
-	public static inline var LINEAR_DISTANCE_CLAMPED:Int = 0xD004;
-	public static inline var EXPONENT_DISTANCE:Int = 0xD005;
-	public static inline var EXPONENT_DISTANCE_CLAMPED:Int = 0xD006;
+	public static inline final NONE:Int = 0;
+	public static inline final FALSE:Int = 0;
+	public static inline final TRUE:Int = 1;
+	public static inline final SOURCE_RELATIVE:Int = 0x202;
+	public static inline final CONE_INNER_ANGLE:Int = 0x1001;
+	public static inline final CONE_OUTER_ANGLE:Int = 0x1002;
+	public static inline final PITCH:Int = 0x1003;
+	public static inline final POSITION:Int = 0x1004;
+	public static inline final DIRECTION:Int = 0x1005;
+	public static inline final VELOCITY:Int = 0x1006;
+	public static inline final LOOPING:Int = 0x1007;
+	public static inline final BUFFER:Int = 0x1009;
+	public static inline final GAIN:Int = 0x100A;
+	public static inline final MIN_GAIN:Int = 0x100D;
+	public static inline final MAX_GAIN:Int = 0x100E;
+	public static inline final ORIENTATION:Int = 0x100F;
+	public static inline final SOURCE_STATE:Int = 0x1010;
+	public static inline final INITIAL:Int = 0x1011;
+	public static inline final PLAYING:Int = 0x1012;
+	public static inline final PAUSED:Int = 0x1013;
+	public static inline final STOPPED:Int = 0x1014;
+	public static inline final BUFFERS_QUEUED:Int = 0x1015;
+	public static inline final BUFFERS_PROCESSED:Int = 0x1016;
+	public static inline final REFERENCE_DISTANCE:Int = 0x1020;
+	public static inline final ROLLOFF_FACTOR:Int = 0x1021;
+	public static inline final CONE_OUTER_GAIN:Int = 0x1022;
+	public static inline final MAX_DISTANCE:Int = 0x1023;
+	public static inline final SEC_OFFSET:Int = 0x1024;
+	public static inline final SAMPLE_OFFSET:Int = 0x1025;
+	public static inline final BYTE_OFFSET:Int = 0x1026;
+	public static inline final SOURCE_TYPE:Int = 0x1027;
+	public static inline final STATIC:Int = 0x1028;
+	public static inline final STREAMING:Int = 0x1029;
+	public static inline final UNDETERMINED:Int = 0x1030;
+	public static inline final FORMAT_MONO8:Int = 0x1100;
+	public static inline final FORMAT_MONO16:Int = 0x1101;
+	public static inline final FORMAT_STEREO8:Int = 0x1102;
+	public static inline final FORMAT_STEREO16:Int = 0x1103;
+	public static inline final FREQUENCY:Int = 0x2001;
+	public static inline final BITS:Int = 0x2002;
+	public static inline final CHANNELS:Int = 0x2003;
+	public static inline final SIZE:Int = 0x2004;
+	public static inline final UNUSED:Int = 0x2010;
+	public static inline final PENDING:Int = 0x2011;
+	public static inline final PROCESSED:Int = 0x2012;
+	public static inline final NO_ERROR:Int = 0;
+	public static inline final INVALID_NAME:Int = 0xA001;
+	public static inline final INVALID_ENUM:Int = 0xA002;
+	public static inline final INVALID_VALUE:Int = 0xA003;
+	public static inline final INVALID_OPERATION:Int = 0xA004;
+	public static inline final OUT_OF_MEMORY:Int = 0xA005;
+	public static inline final VENDOR:Int = 0xB001;
+	public static inline final VERSION:Int = 0xB002;
+	public static inline final RENDERER:Int = 0xB003;
+	public static inline final EXTENSIONS:Int = 0xB004;
+	public static inline final DOPPLER_FACTOR:Int = 0xC000;
+	public static inline final DOPPLER_VELOCITY:Int = 0xC001;
+	public static inline final SPEED_OF_SOUND:Int = 0xC003;
+	public static inline final DISTANCE_MODEL:Int = 0xD000;
+	public static inline final INVERSE_DISTANCE:Int = 0xD001;
+	public static inline final INVERSE_DISTANCE_CLAMPED:Int = 0xD002;
+	public static inline final LINEAR_DISTANCE:Int = 0xD003;
+	public static inline final LINEAR_DISTANCE_CLAMPED:Int = 0xD004;
+	public static inline final EXPONENT_DISTANCE:Int = 0xD005;
+	public static inline final EXPONENT_DISTANCE_CLAMPED:Int = 0xD006;
 
 	// functions //
 	@:native('alGetError')
@@ -101,7 +101,7 @@ extern class AL {
 	public static function distanceModel(distanceModel:Int):Void;
 
 	@:native('alGetString')
-	public static function getString(parameter:Int):String;
+	public static function getString(parameter:Int):cpp.ConstCharStar;
 
 	@:native('alGetBooleanv')
 	public static function getBooleanv(parameter:Int, value:cpp.Pointer<Bool>):Void;
@@ -128,13 +128,13 @@ extern class AL {
 	public static function getDouble(parameter:Int):Double;
 
 	@:native('alIsExtensionPresent')
-	public static function isExtensionPresent(extension:String):Bool;
+	public static function isExtensionPresent(extension:cpp.ConstCharStar):Bool;
 
 	@:native('alGetProcAddress')
-	public static function getProcAddress(func:String):cpp.Pointer<Void>;
+	public static function getProcAddress(func:cpp.ConstCharStar):cpp.Pointer<cpp.Void>;
 
 	@:native('alGetEnumValue')
-	public static function getEnumValue(enumName:String):Int;
+	public static function getEnumValue(enumName:cpp.ConstCharStar):Int;
 
 	@:native('alListenerf')
 	public static function listenerf(parameter:Int, value:Single):Void;
@@ -178,78 +178,78 @@ extern class AL {
 	}
 
 	@:native('alDeleteSources')
-	public inline static function deleteSources(n:Int, source:UInt):Void {
+	public inline static function deleteSources(n:Int, source:cpp.UInt32):Void {
 		HxAL.deleteSources(n, source);
 	};
 
 	@:native('alIsSource')
-	public static function isSource(source:UInt):Bool;
+	public static function isSource(source:cpp.UInt32):Bool;
 
 	@:native('alSourcef')
-	public static function sourcef(source:UInt, parameter:Int, value:Single):Void;
+	public static function sourcef(source:cpp.UInt32, parameter:Int, value:Single):Void;
 
 	@:native('alSource3f')
-	public static function source3f(source:UInt, parameter:Int, value1:Single, value2:Single, value3:Single):Void;
+	public static function source3f(source:cpp.UInt32, parameter:Int, value1:Single, value2:Single, value3:Single):Void;
 
 	@:native('alSourcefv')
-	public static function sourcefv(source:UInt, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
+	public static function sourcefv(source:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
 
 	@:native('alSourcei')
-	public static function sourcei(source:UInt, parameter:Int, value:Int):Void;
+	public static function sourcei(source:cpp.UInt32, parameter:Int, value:Int):Void;
 
 	@:native('alSource3i')
-	public static function source3i(source:UInt, parameter:Int, value1:Int, value2:Int, value3:Int):Void;
+	public static function source3i(source:cpp.UInt32, parameter:Int, value1:Int, value2:Int, value3:Int):Void;
 
 	@:native('alSourceiv')
-	public static function sourceiv(source:UInt, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
+	public static function sourceiv(source:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
 
 	@:native('alGetSourcef')
-	public static function getSourcef(source:UInt, parameter:Int, value:cpp.Pointer<Single>):Void;
+	public static function getSourcef(source:cpp.UInt32, parameter:Int, value:cpp.Pointer<Single>):Void;
 
 	@:native('alGetSource3f')
-	public static function getSource3f(source:UInt, parameter:Int, value1:cpp.Pointer<Single>, value2:cpp.Pointer<Single>, value3:cpp.Pointer<Single>):Void;
+	public static function getSource3f(source:cpp.UInt32, parameter:Int, value1:cpp.Pointer<Single>, value2:cpp.Pointer<Single>, value3:cpp.Pointer<Single>):Void;
 
 	@:native('alGetSourcefv')
-	public static function getSourcefv(source:UInt, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
+	public static function getSourcefv(source:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
 
 	@:native('alGetSourcei')
-	public static function getSourcei(source:UInt, parameter:Int, value:cpp.Pointer<Int>):Void;
+	public static function getSourcei(source:cpp.UInt32, parameter:Int, value:cpp.Pointer<Int>):Void;
 
 	@:native('alGetSource3i')
-	public static function getSource3i(source:UInt, parameter:Int, value1:cpp.Pointer<Int>, value2:cpp.Pointer<Int>, value3:cpp.Pointer<Int>):Void;
+	public static function getSource3i(source:cpp.UInt32, parameter:Int, value1:cpp.Pointer<Int>, value2:cpp.Pointer<Int>, value3:cpp.Pointer<Int>):Void;
 
 	@:native('alGetSourceiv')
-	public static function getSourceiv(source:UInt, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
+	public static function getSourceiv(source:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
 
 	@:native('alSourcePlay')
-	public static function sourcePlay(source:UInt):Void;
+	public static function sourcePlay(source:cpp.UInt32):Void;
 
 	@:native('alSourceStop')
-	public static function sourceStop(source:UInt):Void;
+	public static function sourceStop(source:cpp.UInt32):Void;
 
 	@:native('alSourceRewind')
-	public static function sourceRewind(source:UInt):Void;
+	public static function sourceRewind(source:cpp.UInt32):Void;
 
 	@:native('alSourcePause')
-	public static function sourcePause(source:UInt):Void;
+	public static function sourcePause(source:cpp.UInt32):Void;
 
 	@:native('alSourcePlayv')
-	public static function sourcePlayv(n:Int, sources:cpp.Pointer<UInt>):Void;
+	public static function sourcePlayv(n:Int, sources:cpp.Pointer<cpp.UInt32>):Void;
 
 	@:native('alSourceStopv')
-	public static function sourceStopv(n:Int, sources:cpp.Pointer<UInt>):Void;
+	public static function sourceStopv(n:Int, sources:cpp.Pointer<cpp.UInt32>):Void;
 
 	@:native('alSourceRewindv')
-	public static function sourceRewindv(n:Int, sources:cpp.Pointer<UInt>):Void;
+	public static function sourceRewindv(n:Int, sources:cpp.Pointer<cpp.UInt32>):Void;
 
 	@:native('alSourcePausev')
-	public static function sourcePausev(n:Int, sources:cpp.Pointer<UInt>):Void;
+	public static function sourcePausev(n:Int, sources:cpp.Pointer<cpp.UInt32>):Void;
 
 	@:native('alSourceQueueBuffers')
-	public static function sourceQueueBuffers(source:UInt, nb:Int, buffers:cpp.Pointer<UInt>):Void;
+	public static function sourceQueueBuffers(source:cpp.UInt32, nb:Int, buffers:cpp.Pointer<cpp.UInt32>):Void;
 
 	@:native('alSourceUnqueueBuffers')
-	public static function sourceUnqueueBuffers(source:UInt, nb:Int, buffers:cpp.Pointer<UInt>):Void;
+	public static function sourceUnqueueBuffers(source:cpp.UInt32, nb:Int, buffers:cpp.Pointer<cpp.UInt32>):Void;
 
 	@:native('alGenBuffers')
 	public static inline function genBuffers(n:Int, buffers:Array<Int>):Void {
@@ -257,64 +257,64 @@ extern class AL {
 	}
 
 	@:native('alDeleteBuffers')
-	public inline static function deleteBuffers(n:Int, buffer:UInt):Void {
+	public inline static function deleteBuffers(n:Int, buffer:cpp.UInt32):Void {
 		HxAL.deleteBuffers(n, buffer);
 	};
 
 	@:native('alIsBuffer')
-	public static function isBuffer(buffer:UInt):Bool;
+	public static function isBuffer(buffer:cpp.UInt32):Bool;
 
 	@:native('alBufferData')
-	private static function alBufferData(buffer:Int, format:Int, data:cpp.Pointer<cpp.UInt8>, size:Int, sampleRate:Int):Void;
+	private static function alBufferData(buffer:Int, format:Int, data:cpp.Pointer<cpp.cpp.UInt328>, size:Int, sampleRate:Int):Void;
 
 	public static inline function bufferData(buffer:Int, format:Int, data:haxe.io.BytesData, size:Int, sampleRate:Int):Void {
 		alBufferData(buffer, format, cpp.Pointer.arrayElem(data, 0), size, sampleRate);
 	}
 
 	@:native('alBufferf')
-	public static function bufferf(buffer:UInt, parameter:Int, value:Single):Void;
+	public static function bufferf(buffer:cpp.UInt32, parameter:Int, value:Single):Void;
 
 	@:native('alBuffer3f')
-	public static function buffer3f(buffer:UInt, parameter:Int, value1:Single, value2:Single, value3:Single):Void;
+	public static function buffer3f(buffer:cpp.UInt32, parameter:Int, value1:Single, value2:Single, value3:Single):Void;
 
 	@:native('alBufferfv')
-	public static function bufferfv(buffer:UInt, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
+	public static function bufferfv(buffer:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
 
 	@:native('alBufferi')
-	public static function bufferi(buffer:UInt, parameter:Int, value:Int):Void;
+	public static function bufferi(buffer:cpp.UInt32, parameter:Int, value:Int):Void;
 
 	@:native('alBuffer3i')
-	public static function buffer3i(buffer:UInt, parameter:Int, value1:Int, value2:Int, value3:Int):Void;
+	public static function buffer3i(buffer:cpp.UInt32, parameter:Int, value1:Int, value2:Int, value3:Int):Void;
 
 	@:native('alBufferiv')
-	public static function bufferiv(buffer:UInt, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
+	public static function bufferiv(buffer:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
 
 	@:native('alGetBufferf')
-	public static function getBufferf(buffer:UInt, parameter:Int, value:cpp.Pointer<Single>):Void;
+	public static function getBufferf(buffer:cpp.UInt32, parameter:Int, value:cpp.Pointer<Single>):Void;
 
 	@:native('alGetBuffer3f')
-	public static function getBuffer3f(buffer:UInt, parameter:Int, value1:cpp.Pointer<Single>, value2:cpp.Pointer<Single>, value3:cpp.Pointer<Single>):Void;
+	public static function getBuffer3f(buffer:cpp.UInt32, parameter:Int, value1:cpp.Pointer<Single>, value2:cpp.Pointer<Single>, value3:cpp.Pointer<Single>):Void;
 
 	@:native('alGetBufferfv')
-	public static function getBufferfv(buffer:UInt, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
+	public static function getBufferfv(buffer:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Single>>):Void;
 
 	@:native('alGetBufferi')
-	public static function getBufferi(buffer:UInt, parameter:Int, value:cpp.Pointer<Int>):Void;
+	public static function getBufferi(buffer:cpp.UInt32, parameter:Int, value:cpp.Pointer<Int>):Void;
 
 	@:native('alGetBuffer3i')
-	public static function getBuffer3i(buffer:UInt, parameter:Int, value1:cpp.Pointer<Int>, value2:cpp.Pointer<Int>, value3:cpp.Pointer<Int>):Void;
+	public static function getBuffer3i(buffer:cpp.UInt32, parameter:Int, value1:cpp.Pointer<Int>, value2:cpp.Pointer<Int>, value3:cpp.Pointer<Int>):Void;
 
 	@:native('alGetBufferiv')
-	public static function getBufferiv(buffer:UInt, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
+	public static function getBufferiv(buffer:cpp.UInt32, parameter:Int, values:cpp.Pointer<Array<Int>>):Void;
 }
 
 @:include('AL/hxal.h')
 extern class HxAL {
 	@:native('deleteSourceHelper')
-	public static function deleteSources(n:Int, source:UInt):Void;
+	public static function deleteSources(n:Int, source:cpp.UInt32):Void;
 
 	@:native('deleteBufferHelper')
-	public static function deleteBuffers(n:Int, buffer:UInt):Void;
+	public static function deleteBuffers(n:Int, buffer:cpp.UInt32):Void;
 }
 
 typedef Double = cpp.Float64;
--- src/al/ALC.hx
+++ src/al/ALC.hx
@@ -12,35 +12,35 @@ import cpp.Pointer;
 @:include('AL/alc.h')
 extern class ALC {
 	// defines //
-	public static inline var FREQUENCY:Int = 0x1007;
-	public static inline var REFRESH:Int = 0x1008;
-	public static inline var SYNC:Int = 0x1009;
-	public static inline var MONO_SOURCES:Int = 0x1010;
-	public static inline var STEREO_SOURCES:Int = 0x1011;
-	public static inline var NO_ERROR:Int = 0;
-	public static inline var INVALID_DEVICE:Int = 0xA001;
-	public static inline var INVALID_CONTEXT:Int = 0xA002;
-	public static inline var INVALID_ENUM:Int = 0xA003;
-	public static inline var INVALID_VALUE:Int = 0xA004;
-	public static inline var OUT_OF_MEMORY:Int = 0xA005;
-	public static inline var MAJOR_VERSION:Int = 0x1000;
-	public static inline var MINOR_VERSION:Int = 0x1001;
-	public static inline var ATTRIBUTES_SIZE:Int = 0x1002;
-	public static inline var ALL_ATTRIBUTES:Int = 0x1003;
-	public static inline var DEFAULT_DEVICE_SPECIFIER:Int = 0x1004;
-	public static inline var DEVICE_SPECIFIER:Int = 0x1005;
-	public static inline var EXTENSIONS:Int = 0x1006;
-	public static inline var EXT_CAPTURE:Int = 1;
-	public static inline var CAPTURE_DEVICE_SPECIFIER:Int = 0x310;
-	public static inline var CAPTURE_DEFAULT_DEVICE_SPECIFIER:Int = 0x311;
-	public static inline var CAPTURE_SAMPLES:Int = 0x312;
-	public static inline var ENUMERATE_ALL_EXT:Int = 1;
-	public static inline var DEFAULT_ALL_DEVICES_SPECIFIER:Int = 0x1012;
-	public static inline var ALL_DEVICES_SPECIFIER:Int = 0x1013;
+	public static inline final FREQUENCY:Int = 0x1007;
+	public static inline final REFRESH:Int = 0x1008;
+	public static inline final SYNC:Int = 0x1009;
+	public static inline final MONO_SOURCES:Int = 0x1010;
+	public static inline final STEREO_SOURCES:Int = 0x1011;
+	public static inline final NO_ERROR:Int = 0;
+	public static inline final INVALID_DEVICE:Int = 0xA001;
+	public static inline final INVALID_CONTEXT:Int = 0xA002;
+	public static inline final INVALID_ENUM:Int = 0xA003;
+	public static inline final INVALID_VALUE:Int = 0xA004;
+	public static inline final OUT_OF_MEMORY:Int = 0xA005;
+	public static inline final MAJOR_VERSION:Int = 0x1000;
+	public static inline final MINOR_VERSION:Int = 0x1001;
+	public static inline final ATTRIBUTES_SIZE:Int = 0x1002;
+	public static inline final ALL_ATTRIBUTES:Int = 0x1003;
+	public static inline final DEFAULT_DEVICE_SPECIFIER:Int = 0x1004;
+	public static inline final DEVICE_SPECIFIER:Int = 0x1005;
+	public static inline final EXTENSIONS:Int = 0x1006;
+	public static inline final EXT_CAPTURE:Int = 1;
+	public static inline final CAPTURE_DEVICE_SPECIFIER:Int = 0x310;
+	public static inline final CAPTURE_DEFAULT_DEVICE_SPECIFIER:Int = 0x311;
+	public static inline final CAPTURE_SAMPLES:Int = 0x312;
+	public static inline final ENUMERATE_ALL_EXT:Int = 1;
+	public static inline final DEFAULT_ALL_DEVICES_SPECIFIER:Int = 0x1012;
+	public static inline final ALL_DEVICES_SPECIFIER:Int = 0x1013;
 
 	// functions //
 	@:native('alcOpenDevice')
-	public static function openDevice(?deviceName:String):Device;
+	public static function openDevice(?deviceName:cpp.ConstCharStar):Device;
 
 	@:native('alcCloseDevice')
 	public static function closeDevice(device:Device):Bool;
@@ -70,22 +70,22 @@ extern class ALC {
 	public static function getError(device:Device):Int;
 
 	@:native('alcIsExtensionPresent')
-	public static function isExtensionPresent(device:Device, extension:String):Bool;
+	public static function isExtensionPresent(device:Device, extension:cpp.ConstCharStar):Bool;
 
 	@:native('alcGetProcAddress')
-	public static function getProcAddress(device:Device, func:String):cpp.Pointer<Void>;
+	public static function getProcAddress(device:Device, func:cpp.ConstCharStar):cpp.Pointer<cpp.Void>;
 
 	@:native('alcGetEnumValue')
-	public static function getEnumValue(device:Device, enumName:String):Int;
+	public static function getEnumValue(device:Device, enumName:cpp.ConstCharStar):Int;
 
 	@:native('alcGetString')
-	public static function getString(device:Device, parameter:Int):String;
+	public static function getString(device:Device, parameter:Int):cpp.ConstCharStar;
 
 	@:native('alcGetIntegerv')
 	public static function getIntegerv(device:Device, parameter:Int, size:Int, ?values:cpp.Pointer<Int>):Void;
 
 	@:native('alcCaptureOpenDevice')
-	public static function captureOpenDevice(deviceName:String, frequency:UInt, format:Int, bufferSize:Int):Device;
+	public static function captureOpenDevice(deviceName:cpp.ConstCharStar, frequency:cpp.UInt32, format:Int, bufferSize:Int):Device;
 
 	@:native('alcCaptureCloseDevice')
 	public static function captureCloseDevice(device:Device):Bool;
@@ -97,7 +97,7 @@ extern class ALC {
 	public static function captureStop(device:Device):Void;
 
 	@:native('alcCaptureSamples')
-	public static function captureSamples(device:Device, buffer:cpp.Pointer<Void>, samples:Int):Void;
+	public static function captureSamples(device:Device, buffer:cpp.Pointer<cpp.Void>, samples:Int):Void;
 }
 
 @:native('ALCdevice')
